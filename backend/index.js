e7e40c8bccf45495dc9135acca83773d190d8f75
Subhrajit-14 <subhrajitswain1678@gmail.com>
2024-07-28 13:55:11 +0530

    Final Commit

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+const { OrdersModel } = require("./model/OrdersModel");
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+const app = express();
+const router = require("./router/auth-router");
+const errorMiddleware = require("./middlewares/error.middleware");
+
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET , POST, PUT, DELETE, PATCH, HEAD",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+
+app.use("/api/auth", router);
+app.use(errorMiddleware);
+
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved!");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log("App started!");
+  mongoose.connect(uri);
+  console.log("DB started!");
+});
